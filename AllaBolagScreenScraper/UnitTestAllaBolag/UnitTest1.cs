c210f3284123d0e2a5d1a6c1e239e940e1e2fdef
superdupermartin <martinagnalv@gmail.com>
2014-10-06 14:13:34 +0200

    Jag har skrivit ett bra test. Varsågoda

--- AllaBolagScreenScraper/UnitTestAllaBolag/UnitTest1.cs
+++ AllaBolagScreenScraper/UnitTestAllaBolag/UnitTest1.cs
@@ -1,18 +1,26 @@
-﻿//using System;
-//using System.Text.RegularExpressions;
-//using System.Threading.Tasks;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-//namespace UnitTestAllaBolag
-//{
-//    [TestClass]
-//    public class ScreenScraper
-//    {
-//        [TestMethod]
-//        public Task Start()
-//        {
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AllaBolagScreenScraper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
-//        }
-//    }
-//}
+namespace UnitTestAllaBolag
+{
+    [TestClass]
+    public class ScreenScraper
+    {
+        [TestMethod]
+        public void TestUpplysningReturnsCorrectCompanyMame()
+        {
+            //Arrange
+           ScreenScraperUpplysning scr = new ScreenScraperUpplysning();
+            string expexted = "Mattias Asplund Aktiebolag";
+            //Act
+            Task<string> actual = scr.ScreenScrapeAsync("5565995239");
+            string actual1 = actual.Result;
+            //Assert
+            Assert.AreEqual(expexted, actual1);
+        }
+    }
+}
